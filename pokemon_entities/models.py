c1fd54bec6f29f443717a82ceab844f754d160c9
evgenok12 <evgenok12@gmail.com>
2023-04-13 16:30:10 +0300

    add some fields to Pokemon model, show them on pokemon page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/pokemon', blank=True)
+    title_ru = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200)
+    title_jp = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
 
     def __str__(self):
-        return self.title
+        return self.title_ru
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT, related_name='pokemon_entities')
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField()
