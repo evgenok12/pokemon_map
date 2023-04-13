ca4c48f50076d6a3127200e78f71ac8d5e682b35
evgenok12 <evgenok12@gmail.com>
2023-04-13 17:01:58 +0300

    fix import

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,7 @@
 import folium
 from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
-from .models import *
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
